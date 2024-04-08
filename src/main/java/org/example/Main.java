2bf2c00be51d42837e15bb1664be5d8922caa70c
miros <relenci095@gmail.com>
2024-04-08 20:01:01 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+import com.google.gson.Gson;
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Name";
+        String lastName = "Last Name";
+
+        Gson gson = new Gson();
+        String json = gson.toJson(new NameObject(name, lastName));
+        System.out.println(json);
+
+    }
+
+    static class NameObject{
+        private String name;
+        private String lastName;
+
+        public class NameObject(String name, String lastName){
+            this.name = name;
+            this.lastName = lastName;
+        }
+    }
+}
